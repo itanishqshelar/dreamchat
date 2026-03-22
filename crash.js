ea7b2a1d814c309df6dda8642f3799cb5e92fc59
tanishelar <tanishq.shelar91@gmail.com>
2026-03-22 12:35:10 +0530

    Remove unnecessary build error logs and crash files

--- crash.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-try {
-  const adb = process.env.LOCALAPPDATA + '\\Android\\Sdk\\platform-tools\\adb.exe';
-  const result = execSync(`"${adb}" logcat -d -b crash`).toString('utf8');
-  console.log(result.slice(-2000));
-} catch (e) {
-  console.log(e.toString());
-}
